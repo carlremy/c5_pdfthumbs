12c1929ca609bbaa2dfc428b1c7167bbe4f27306
Carlton Remy <carlton@remy.net>
2015-10-31 21:34:37 -0700

    interface updates for edit form

--- blocks/pdf_thumb/edit_form.php
+++ blocks/pdf_thumb/edit_form.php
@@ -3,21 +3,33 @@ $al = Loader::helper('concrete/asset_library');
 $fh= Loader::helper('form');
 
 ?>
-<div class="ccm-block-field-group">
+<!--div class="ccm-block-field-group"-->
+	
+	<h4><?php echo t('File to Display')?></h4><br>
 
-<div class="row">
-	<div class="span6">
-		<?php echo $al->doc('ccm-b-file', 'fID', t('Choose File'), $fID);?>
-	</div>
-</div>
-
-<hr>
-<div class="row">
-	<div class="span4">
-		<?php echo $fh->label('ccm-b-file', t('Thumbnail Size')) ?>
-		<div class="input">
-			<?php echo $fh->select('thumbnailSize', array(1=>t('Small'), 2=>t('Medium'), 3=>t('Large')), $thumbnailSize, array('class'=>'form-control')) ?>
+	<div class="clearfix">
+		<?php echo $fh->label('ccm-b-file', t('File')) ?>
+		<div class="input">	
+			<?php echo $al->doc('ccm-b-file', 'fID', t('Choose File'), $fID);?>
 		</div>
 	</div>
-</div>
-</div>
+
+	<div class="clearfix">
+		<?php echo $fh->label('ccm-b-image', t('Alternate Image')) ?>
+		<div class="input">	
+			<?php echo $al->image('ccm-b-image', 'altThumbnailfID', t('Choose Image'), $altThumbnailfID);?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $fh->label('thumbnailSize', t('Thumbnail Size')) ?>
+		<div class="input">	
+			<?php echo $fh->select('thumbnailSize', array(1=>t('Small'), 2=>t('Medium'), 3=>t('Large')), $thumbnailSize) ?>
+		</div>
+	</div>
+	
+	<div class="clearfix">	
+		<a href="#" id="btn-refresh-thumbnail" class="btn btn-default">Refresh Thumbnail</a>
+	</div>
+<!--/div-->
+
