03a7cccf945641c934f58a376a788980c4cc8852
carl <Carlton Remy>
2015-10-31 22:00:38 -0700

    bug fix and add ajax response

--- tools/generate.php
+++ tools/generate.php
@@ -1,18 +1,20 @@
 <?php defined('C5_EXECUTE') or die(_("Access denied."));
 header('Content-type:application/json;charset=utf-8');
 
-//Check permissions
-
 $fID = isset($_GET['fID'])?(int)$_GET['fID']:0;
 $file = File::getByID($fID);
 
-
+//Check permissions
 $fp = new Permissions($file);
 if ($fp->canEditFileContents()) { 
 	$fv = $file->getApprovedVersion();
 
 	if($fv->getMimeType() == 'application/pdf') {
-		Loader::helper('pdfthumb', 'pdfthumbs')->generate();	
+		if($success = Loader::helper('pdfthumb', 'pdfthumbs')->generate($fv) ) {
+			Loader::helper('ajax')-> sendResult( array('status'=>'OK', 'src'=>$fv->getThumbnailSRC(3)) );	
+		} else {
+			Loader::helper('ajax')-> sendError( t('Unable to generate PDF Preview.') );	
+		}
 	}
 }
 
